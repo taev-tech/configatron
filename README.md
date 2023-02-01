3ffdd214c87b4a38e30f7134360f9b4300345ed2
Nicholas Badger <nbadger1@gmail.com>
2023-02-01 22:47:24 +0100

    Add cmdline backend

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ General-purpose configuration based on annotations. Uses TOML files for plaintex
 ## A note on inheritance
 
 +   If you really want config inheritance, you should hard code it as an additional backend. So for example, if you wanted to have a default local config with an additional per-developer local config (and add the latter to your gitignore), the way you'd do that is that whatever if block decides which file to use for which environment, also adds a second backend in the local environment for the dev-specific override file
+
+## Questions that I need to answer and document:
+
++   what are allowed values for namespaces?
++   same question, but for key names
++   does the cmdline backend support quoting for values?
---
+++ tests/backends/test_cmdline.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from configatron.backends.cmdline import CmdlineBackend
+from configatron.configatron_loader import ConfigatronLoader
+
+CONFIG_ARGV = [
+    '__main__',
+    '--sample_config_only_unsecured.one=one',
+    '--sample_config_only_unsecured.two=two',
+    '--sample_config_only_unsecured.three=three',
+    '--sample_config_only_unsecured.four=four',
+]
+
+
+@patch('sys.argv', CONFIG_ARGV)
+def test_loading(sample_config_only_unsecured):
+    backend = CmdlineBackend()
+    loader = ConfigatronLoader([backend])
+
+    with loader.load():
+        assert sample_config_only_unsecured.one == 'one'
+        assert sample_config_only_unsecured.two == 'two'
+        assert sample_config_only_unsecured.three == 'three'
+        assert sample_config_only_unsecured.four == 'four'
